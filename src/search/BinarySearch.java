62b089aafea97b9b285281d3b17e691e915784e9
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-10-25 18:17:50 +0900

    BinarySearch Arrays API refactoring

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -4,25 +4,27 @@ import java.util.Arrays;
 
 public class BinarySearch {
 
-    private static int BinarySearchMethod(int[] array, int n, int key) {
-        int pl = 0;
-        int pr = n - 1;
+    private static int BinarySearchMethod(int[] array) {
 
-        do {
-            int pc = (pl+pr) / 2;
-            if(array[pc] == key)
-                return pc;
-            else if(array[pc] < key)
-                pl = pc + 1;
-            else if(array[pc] > key)
-                pr = pc - 1;
-        } while (pl <= pr);
-        return -1;
+        return Arrays.binarySearch(array, 1);
+
+//        int pl = 0;
+//        int pr = n - 1;
+//
+//        do {
+//            int pc = (pl+pr) / 2;
+//            if(array[pc] == key)
+//                return pc;
+//            else if(array[pc] < key)
+//                pl = pc + 1;
+//            else if(array[pc] > key)
+//                pr = pc - 1;
+//        } while (pl <= pr);
+//        return -1;
     }
 
     public static void main(String[] args) {
         int[] array = {1,3,5,7,9,11,13,15,17,19};
-        int result = BinarySearchMethod(array, array.length, 1);
-        System.out.println(result);
+        System.out.println(BinarySearchMethod(array));
     }
 }

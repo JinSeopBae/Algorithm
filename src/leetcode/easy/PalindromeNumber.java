51f9a0ad31ca099bf2c3ffeca7049e7bb3915df1
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-10-26 16:35:52 +0900

    LeetCode ReverseInteger add

---
+++ src/leetcode/easy/PalindromeNumber.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+import java.util.Stack;
+
+public class PalindromeNumber {
+
+    public static boolean palindromeNumber(int x) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
